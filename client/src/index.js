39d527e4d84673ca3e6ad7b9e38b4ade468d7b28
willrock19 <williamporto_14@hotmail.com>
2023-03-22 16:51:27 -0300

    Formatting files after eslint and prettier

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-root.render(<App/>)
+root.render(<App />);
