feab7508888c0d33a2db682bc15726af3716e3a1
willrock19 <williamporto_14@hotmail.com>
2023-03-24 14:25:21 -0300

    Removing unecessary code from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,10 @@
-import { useState } from 'react';
 import GlobalStyle from './GlobalStyles';
-import Button from './components/Button';
-import InformationModal from './components/Information-Modal';
-import { Dropdown } from './components/Dropdown/Dropdown';
-
-const options = [
-	{ value: 1, label: 'One' },
-	{ value: 10, label: 'Ten' },
-	{ value: 100, label: 'Hundred' }
-];
 
 const App = () => {
-	const [showModal, setShowModal] = useState(false);
-
-	const closeModal = () => {
-		setShowModal(false);
-	};
-
-	const dropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		console.log(event.target.value);
-	};
-
 	return (
 		<>
 			<GlobalStyle />
 			<h1>Hello World! I did it. I&apos;ve actually did it!</h1>
-
-			{showModal && (
-				<InformationModal title="Teste" onCloseFn={closeModal}>
-					Este é meu teste
-				</InformationModal>
-			)}
-
-			<Button onClick={() => setShowModal(true)}>Teste</Button>
-			<Dropdown onChange={dropdownChange} options={options}></Dropdown>
 		</>
 	);
 };
